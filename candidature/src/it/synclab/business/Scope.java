a7db25893d77ec5ab17249b578a7ab04596325b8
Deiv <cepics89@gmail.com>
2017-08-09 11:35:48 +0200

    added Scope class and new jsp candidateService and cssFIle from Niko

---
+++ candidature/src/it/synclab/business/Scope.java
@@ -0,0 +1,55 @@
+package it.synclab.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "SCOPE")
+public class Scope {
+	
+	@Id
+	@Column(name = "ID_SCOPE")
+	private int idScope;
+	
+	@Column(name = "SCOPE_NAME")
+	private String scopeName;
+	
+	/*@OneToMany(mappedBy = "idScope", cascade = CascadeType.ALL)
+	private List<Skills> skills = new ArrayList<>();*/
+
+	
+	public int getIdScope() {
+		return idScope;
+	}
+
+	public void setIdScope(int idScope) {
+		this.idScope = idScope;
+	}
+
+	public String getScopeName() {
+		return scopeName;
+	}
+
+	public void setScopeName(String scopeName) {
+		this.scopeName = scopeName;
+	}
+
+	/*public List<Skills> getSkills() {
+		return skills;
+	}
+
+	public void setSkills(List<Skills> skills) {
+		this.skills = skills;
+	}*/
+	
+	
+	
+	
+}
